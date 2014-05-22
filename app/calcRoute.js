44326347c31d65ae010c126cbfcd7be364bd8fb3
Andy Ernst <AndyCErnst@gmail.com>
2014-05-22 12:24:00 -0700

    merge commit

--- app/calcRoute.js
+++ app/calcRoute.js
@@ -1,6 +1,7 @@
 //This function just gives me an overview_path array of points
-//for the route. 
+//for the route.
 function calcRoute(start, end, callback, map) {
+    clearBoxes();
 
     var request = {
         origin: start,
@@ -9,7 +10,7 @@ function calcRoute(start, end, callback, map) {
     };
     new google.maps.DirectionsService().route(request, function(result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-            //call function to map polyline and pass in the polyline 
+            //call function to map polyline and pass in the polyline
             //object from the results
             //result.routes[0].overview_path
 
