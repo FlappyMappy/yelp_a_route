fc03ff6517121f5322386aa0679594cedc1168c1
Hailey Petway <petwayh@gmail.com>
2014-05-21 20:50:18 -0700

    pushing calcRoute with just the route added, couldnt figure out the map clear.

--- calcRoute.js
+++ calcRoute.js
@@ -1,8 +1,7 @@
 //This function just gives me an overview_path array of points
 //for the route.
-function calcRoute(start, end, callback, map) {
 
-    clearMap();
+function calcRoute(start, end, callback, map) {
 
     var request = {
         origin: start,
@@ -12,9 +11,7 @@ function calcRoute(start, end, callback, map) {
     new google.maps.DirectionsService().route(request, function(result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             new google.maps.DirectionsRenderer({map: map})
-              .setDirections(result).setOptions(preserveViewport: false);
-              // we might not need to do .setOptions, as this is said to be centered
-              // zoomed by default.
+              .setDirections(result);
 
             callback(result);
         } else {
