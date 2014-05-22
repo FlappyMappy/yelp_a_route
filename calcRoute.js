5083000847f8e5eb0eb54ced4a514e93caf0ddb1
Hailey Petway <petwayh@gmail.com>
2014-05-21 18:53:38 -0700

    Adding preserveViewport to CalcRoute

--- calcRoute.js
+++ calcRoute.js
@@ -11,7 +11,8 @@ function calcRoute(start, end, callback, map) {
     };
     new google.maps.DirectionsService().route(request, function(result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-            new google.maps.DirectionsRenderer({map: map}).setDirections(result);
+            new google.maps.DirectionsRenderer({map: map})
+              .setDirections(result).setOptions(preserveViewport: true);
             //call function to map polyline and pass in the polyline
             //object from the results
             //result.routes[0].overview_path
