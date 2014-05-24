7edc032f54b81138f47aabbcae594ae92d35738c
Kevin Stephens <kevinmstephens@gmail.com>
2014-05-24 12:41:53 -0700

    added parseFullpath function which returns high resolution path from directions result object route

---
+++ app/polylineMileSplit.js
@@ -0,0 +1,28 @@
+//This function will take a polyline and return a polyline
+//that starts at mile startMile and ends at 
+//endMile
+
+function polylineMileSplit(polyline, startMile, endMile){
+
+	startMeter = milesToMeters(startMile);
+	endMeter = milesToMeters(endMile);
+	newPolyline = [];
+
+	//hold a reference to the method to make it easier to read. Results in meters
+	var distance = google.maps.geometry.spherical.computeDistanceBetween;
+
+	var i;
+	for (i = 0; i < polyline.length; i++) {
+
+		var metersFromPLStartToCurrent = distance(polyline[0], polyline[i]);
+
+		if (metersFromPLStartToCurrent >= startMeter){
+			if (metersFromPLStartToCurrent <= endMeter){
+				newPolyline.push(polyline[i]);
+			} else {
+				return newPolyline;
+			};
+		};
+	};
+	return newPolyline;
+};
