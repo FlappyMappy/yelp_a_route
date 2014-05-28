e63f4e517ab5b2bb6263cb83e74330b26f25f09b
Hailey Petway <petwayh@gmail.com>
2014-05-28 09:57:09 -0700

    adding updates to test dir

--- test/front-end/unit/calcRouteTest.js
+++ test/front-end/unit/calcRouteTest.js
@@ -1,14 +1,20 @@
-var CalcRoute = require('../app/calcRoute');
+var CalcRoute = require('../../../app/calcRoute');
 var expect = require('chai').expect
 
 describe("CalcRoute", function() {
-    var calcRoute;
-    before(function(){
-  calcRoute = new CalcRoute();
-    });
+  var calcRoute;
+  var result;
+  before(function(){
+    calcRoute = new CalcRoute();
+    result = calcRoute("seattle", "tacoma", callback, map);
+  });
 
-  it('returns a result', function() {
-    calcRoute();
-    expect(calcRoute()).to.return(result)
-    });
+  it('return is an object', function() {
+   expect(result.to.be.equal({}));
+ });
+
+  it('')
+  expect(result.Tb.destination).to.be.equal("tacoma"));
 });
+
+//mock a start and an end, make a mock request and test the results should equal etc.
